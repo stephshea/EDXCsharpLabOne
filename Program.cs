0ba9d93c91f37a34c96276bfee88472cd73d8667
Stephanie <stephcshea@gmail.com>
2018-05-18 09:47:24 -0400

    completed Lab1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace EDXLabOne
 {
@@ -6,7 +10,20 @@ namespace EDXLabOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var Car1 = new Car();
+
+            Car1.Color = "Silver";
+            Car1.Year = 2004;
+            Car1.Mileage = 180000;
+
+            Console.WriteLine($"This car is painted {Car1.Color}, was built in {Car1.Year}, and has {Car1.Mileage} miles.");
         }
+    }
+
+    public class Car
+    {
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
     }
 }

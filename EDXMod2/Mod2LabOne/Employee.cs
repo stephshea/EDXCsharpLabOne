6e97689ced665ab47ba804b734ec5873e0dfa0a0
Stephanie <stephcshea@gmail.com>
2018-05-20 17:52:19 -0400

    starting LabOne

---
+++ EDXMod2/Mod2LabOne/Employee.cs
@@ -0,0 +1,9 @@
+namespace Mod2LabOne
+{
+    public class Employee
+    {
+        private string employeeName;
+        private double employeeBaseSalary;
+        private int employeeId;
+    }
+}

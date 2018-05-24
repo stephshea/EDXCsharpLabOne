db9bae5a791cc09ac5a4f73cefac25f578f17d5e
Stephanie <stephcshea@gmail.com>
2018-05-24 13:39:47 -0400

    working on Mod2Lab1

--- EDXMod2/Mod2LabOne/Employee.cs
+++ EDXMod2/Mod2LabOne/Employee.cs
@@ -8,8 +8,88 @@ namespace Mod2LabOne
 {
     public class Employee
     {
+        //creates integer variable called 'employeeCount' and assigns value to 1
+        private static int employeeCount = 1;
+
         private string employeeName;
         private double employeeBaseSalary;
         private int employeeId;
+
+        public string Name
+        {
+            get
+            {
+                return employeeName;
+            }
+
+            set
+            {
+                employeeName = value;
+            }
+        }
+
+        public double BaseSalary
+        {
+            get{
+                return employeeBaseSalary;
+            }
+            set{
+                employeeBaseSalary = value;
+            }
+        }
+ public int ID
+        {
+            get
+            {
+                return employeeId;
+            }
+
+            set
+            {
+                employeeId = value;
+            }
+        }
+
+
+        //Constructor
+        public Employee(String name, double baseSalary)
+        {
+            this.Name = name;
+            this.BaseSalary = baseSalary;
+            this.ID = employeeCount++;
+        } 
+
+        //this method returns the employee's base salary
+        public double getBaseSalary()
+        {
+            return this.BaseSalary;
+        }
+
+        //this method returns the employee's name
+        public String getName()
+        {
+            return this.Name;
+        }
+
+         public int getEmployeeID()
+        {
+            return this.ID;
+        }
+
+        //This method returns the employee's ID and Name
+        public String toString()
+        {
+            return this.ID + " " + this.Name;
+        }
+
+        //this method returns ther employee's ID and NAme and confirms that the employee is in the system
+
+        public virtual String employeeStatus()
+        {
+            return toString() + " is in the company's system";
+        }
+
+        }
+
     }
 }
---
+++ EDXMod2/Mod2LabOne/TechnicalEmployee.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2LabOne
+{
+    public class TechnicalEmployee: Employee
+    {
+        //calls upon base-class (Employee Clas) constructor from within derived class(TechnicalEmployee Class)
+        //Sets baseSalary to 75000 for all TechnicalEmployee objects
+
+        public TechnicalEmployee(String name) : base(name, 75000)
+        {
+            //create integer variable called "successfulCheckins" and assigns value to 5
+            public int successfulCheckins = 5;
+        }
+
+        //this method returns the toString() method - which is the employee's ID number and name - and prints the number of successful check ins the employee has had
+
+        public override String employeeStatus()
+        {
+            return this.toString() + " has " + this.successfulCheckins + " successful check ins";
+        }
+    }
+}
